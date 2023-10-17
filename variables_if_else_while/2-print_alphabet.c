b4372b41237cc40880a899430dfb508c7b32e18e
Nassim33150 <abaida.nassim33@gmail.com>
2023-10-17 02:50:30 -0700

    alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int i;
+	
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+		putchar('\n');
+	}
+	return (0);
+}
+
+
