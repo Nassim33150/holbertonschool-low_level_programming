406aef6620af4fbc5e2f1c68b17fc4139cb4ca88
Nassim33150 <abaida.nassim33@gmail.com>
2023-10-17 04:06:51 -0700

    alphas

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -10,9 +10,11 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
+	int i;
+	
 	for (i = 97; i < 123; i++)
 	{
-		putchar([i]);
+		putchar(i);
 	}
 	for (i = 65; i < 90; i++)
 	{
