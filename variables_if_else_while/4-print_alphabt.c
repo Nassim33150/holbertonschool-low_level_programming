1a8f05180cb978f9f6e20a251a29cb1007029035
Nassim33150 <abaida.nassim33@gmail.com>
2023-10-17 04:44:29 -0700

    except

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -8,20 +8,14 @@
 int main(void)
 {
 	int i;
-
-	for (i = 97; i = 123; i++) 
+	
+	for(i = 97; i < 123; i++) 
 	{
-		if (i != 65 && i != 71)
+		if (i != 101  && i != 113)
 		{
-			putchar("[i]");
-			putchar("\n");
+			putchar(i);
 		}
-		
-		
-			
-		
-
+	}
+	putchar('\n');
 	return (0);
 }
-	
-	
