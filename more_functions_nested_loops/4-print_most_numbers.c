fe00ca4ec9831ddf8a9642230561c8fd5875433d
Nassim33150 <abaida.nassim33@gmail.com>
2023-10-20 08:18:30 -0700

    print

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*print_most_numbers - prints numbers except 2 and 4
+*
+*Return: returns nothing
+*/
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != '2' && i != '9')
+		{
+			_putchar(i);
+		}
+
+	}
+	_putchar('\n');
+}
