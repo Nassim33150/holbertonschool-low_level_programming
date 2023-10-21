a6bdad7d53038eb548de8b89c9e85b7b85db1072
Nassim33150 <abaida.nassim33@gmail.com>
2023-10-21 06:54:55 -0700

    more

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*more_numbers - prints numbers to 14
+*
+*Return: returns nothing
+*/
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 14; i++)
+{
+	_putchar(i + '0');
+}
+	_putchar('\n');
+}
+
