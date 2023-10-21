5cd8d9d08731a4fa9c59bf9bbeb8f1bbe4a14379
Nassim33150 <abaida.nassim33@gmail.com>
2023-10-21 09:58:33 -0700

    print

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -8,33 +8,17 @@ void print_line(int n)
 {
 	int i;
 
-	if (n < 0)
+	if (n <= 0)
 	{
-		for (i = 0; i >= n; i--)
-			{
-			if (n == 0)
-			{
-				_putchar('\n');
-			}
-			else
-			{
-				_putchar('_');
-			}
-			}
+		_putchar('\n');
 	}
-	else if (n > 0)
+	else
 	{
 		for (i = 0; i <= n; i++)
-			{
-			if (n == 0)
-			{
-				_putchar('\n');
-			}
-			else
-			{
-				_putchar('_');
-			}
-			}
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
 	}
 }
 
