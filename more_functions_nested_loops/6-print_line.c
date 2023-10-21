1a9dfc99e11105edead858d6fb7d9eebf99254a5
Nassim33150 <abaida.nassim33@gmail.com>
2023-10-21 09:39:37 -0700

    print_line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+*print_line - prints a straight line
+*@n:parameter
+*Return:returns nothing
+*/
+void print_line(int n)
+{
+	int i;
+
+	if (n < 0)
+	{
+		for (i = 0; i >= n; i--)
+			{
+			if (n == 0)
+			{
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar('_');
+				_putchar('\n');
+			}
+			}
+	}
+	else if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+			{
+			if (n == 0)
+			{
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar('_');
+				_putchar('\n');
+			}
+			}
+	}
+}
+
+
