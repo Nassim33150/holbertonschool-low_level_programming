b01c4ae26dd55a0073fbf361d94412a1c954224b
Nassim33150 <abaida.nassim33@gmail.com>
2023-10-22 01:26:02 -0700

    square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+*print_square - prints squares
+*@size: parameter
+*Return:returns nothing
+*/
+void print_square(int size)
+{
+	int i, n;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (n = 0; n < i; n++)
+			{
+				_putchar('#');
+				_putchar('#');
+
+				_putchar('\n');
+			}
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
