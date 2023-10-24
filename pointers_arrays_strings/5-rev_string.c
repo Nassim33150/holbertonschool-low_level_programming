5d281b2bb373e02a4e4f223796d8e6100a626ee4
Nassim33150 <abaida.nassim33@gmail.com>
2023-10-24 04:26:48 -0700

    reverse

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ */
+void rev_string(char *s)
+{
+	int x, size = 0;
+	int start;
+	int end;
+	int hoolder;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		size++;
+	}
+
+	end = size - 1;
+
+	for (start = 0; start < (size / 2); start++)
+	{
+		hoolder = s[start];
+		s[start] = s[end];
+		s[end] = hoolder;
+	}
+}
