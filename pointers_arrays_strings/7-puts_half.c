c8b9d67bc498decc320d8429fb376e45f69481fc
Nassim33150 <abaida.nassim33@gmail.com>
2023-10-24 07:14:37 -0700

    putshalf

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * followed by a new line
+ * @str: string to be printed
+ */
+void puts_half(char *str)
+{
+	int i, middle;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		;
+	}
+
+	middle = (i) / 2;
+
+	for (; str[middle] <= i; middle++)
+	{
+		_putchar(str[middle]);
+	}
+}
