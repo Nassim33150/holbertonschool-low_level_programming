8605bd8f857a68ed327d2e67a2882f1485210107
Nassim33150 <abaida.nassim33@gmail.com>
2023-10-25 05:44:53 -0700

    arrays

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -11,16 +11,12 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
-		if (i < n)
-		{
-			printf("%d, ", a[i]);
-		}
-		else
-		{
+		if (i == 0)
 			printf("%d", a[i]);
-		}
-		printf("\n");
+		else
+			printf(", %d", a[i]);
 	}
+		printf("\n");
 }
