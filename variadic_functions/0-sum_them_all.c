16cc31394fe5c16013ba70ee1cdc045a0c0d618d
Nassim33150 <abaida.nassim33@gmail.com>
2023-11-17 01:57:21 -0800

    sum

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - sums all arguments passed
+ *
+ * @n: number of arguments
+ *
+ * Return: sum of arguments
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+	int sum = 0;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+	va_end(args);
+	return (sum);
+}
