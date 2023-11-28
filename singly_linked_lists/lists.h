5b0b3c1d613e882c68559985c38dd2d3f5dda9e2
Nassim33150 <abaida.nassim33@gmail.com>
2023-11-28 02:01:55 -0800

    return list len

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,3 +1,7 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stddef.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -14,3 +18,5 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+#endif
