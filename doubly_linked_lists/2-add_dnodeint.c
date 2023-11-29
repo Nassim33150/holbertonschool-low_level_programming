9761db6de5d12c2ae7325191856bc7cf2d11a4b0
Nassim33150 <abaida.nassim33@gmail.com>
2023-11-29 03:05:14 -0800

    adddnodeint

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+  * add_dnodeint - add new nodes to the list
+  * @n: value to add to the list
+  * @head: current place in the list
+  * Return: pointer to current position in list
+  */
+
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
