00940e0e231ea3a02c41899b621522cdb4dd1793
Nassim33150 <abaida.nassim33@gmail.com>
2023-11-29 00:18:20 -0800

    delete whitespaces

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,15 +8,15 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t count = 0; 
-	
+	size_t count = 0;
+
 	if (h->str == NULL)
 		printf("[0] (nil)");
 
 	while (h != NULL)
 	{
 		printf("[%u] %s\n", h->len, h->str);
-		count ++;
+		count++;
 		h = h->next;
 	}
 	return (count);
