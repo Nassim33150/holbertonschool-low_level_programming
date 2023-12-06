0a1e95369c163125e933d73766f981d789ffa408
Nassim33150 <abaida.nassim33@gmail.com>
2023-12-06 05:48:14 -0800

    append to file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -23,7 +23,11 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 	else
 	{
-		return (1);
+		if (filename != NULL)
+			return (1);
+		
+		else
+			return (-1);
 	}
 
 	fd = open(filename, O_APPEND | O_WRONLY);
