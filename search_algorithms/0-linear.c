ca02f88c100704c354cf4d9d208cab4fc865e3bc
root <root@LAPTOP-2LV4F0PM>
2024-01-17 13:52:04 +0100

    indentations

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,24 +1,34 @@
 #include "search_algos.h"
-
+/*
+ * binary_search - Performs a binary search on a sorted array.
+ * @array: Pointer to the sorted array of integers.
+ * @size: Size of the array.
+ * @value: Value to search for in the array.
+ *
+ * Returns the index of the value in the array if present,
+ * otherwise returns -1. The search is performed iteratively,
+ * adjusting the search range based on comparisons with the middle
+ * element until the value is found or the range becomes empty.
+ */
 int linear_search(int *array, size_t size, int value)
 {
-    int index = -1;
+	int index = -1;
 
-    size_t i;
+	size_t i;
 
-    if (array == NULL)
-    {
-        return -1;
-    }
+	if (array == NULL)
+	{
+		return (-1);
+	}
 
-    for (i = 0; i < size; i++)
-    {
-        index++;
-        if (value == array[i])
-        {
-            return index;
-        }
-    }
+	for (i = 0; i < size; i++)
+	{
+		index++;
+		if (value == array[i])
+		{
+			return (index);
+		}
+	}
 
-    return -1;
+	return (-1);
 }
