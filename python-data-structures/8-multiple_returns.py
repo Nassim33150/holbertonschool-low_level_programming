5b56d4103854454418aa2fb6004d851474ba75dc
root <root@LAPTOP-2LV4F0PM>
2024-01-30 13:54:50 +0100

    multiple

--- python-data-structures/8-multiple_returns.py
+++ python-data-structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-#return a tuple with the length of a string and its first character
+# return a tuple with the length of a string and its first character
 def multiple_returns(sentence):
-    if not sentence:
-        sentence[0] == None
+    if sentence is None or len(sentence) == 0:
+        return (0, None)
     result_tuple = (len(sentence), sentence[0])
     return result_tuple
